4b0a7255899380317e7db7d6b585ea770adce8a5
Letti214 <leticia.dani214@gmail.com>
2024-02-29 16:55:45 +0100

    3. gyak random színű villogó gombok, változtatható számokkal

---
+++ Gyak3/SzamoloGomb.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gyak3
+{
+    internal class SzamoloGomb : VillogoGomb
+    {
+        int i = 1;
+        public SzamoloGomb()
+        {
+            MouseClick += SzamoloGomb_MouseClick;
+            Text = i.ToString();
+        }
+
+        private void SzamoloGomb_MouseClick(object? sender, MouseEventArgs e)
+        {
+            if(i==10) { i=0; }
+            i++;
+            Text = i.ToString();
+        }
+    }
+}
