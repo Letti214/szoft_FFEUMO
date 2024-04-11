035b90a780b330cc29c2e1fd0e6fdbb6745f16f2
Letti214 <leticia.dani214@gmail.com>
2024-04-11 16:58:06 +0200

    Adatkötés, gyakorlás ZH2re

---
+++ Adatkotes/Form1.cs
@@ -0,0 +1,45 @@
+using CsvHelper;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace Adatkotes
+{
+    public partial class Form1 : Form
+    {
+        BindingList<CountryData> countryList = new();
+        public Form1()
+        {
+            InitializeComponent();
+            countryDataBindingSource.DataSource = countryList;
+            //dataGridView1.DataSource = countryDataBindingSource;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("european_countries.csv");
+            var csv = new CsvReader(sr, CultureInfo.InvariantCulture);
+            var tömb = csv.GetRecords<CountryData>();
+            foreach (var item in tömb)
+            {
+                countryList.Add(item);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            countryDataBindingSource.RemoveCurrent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FormCountryEdit fce = new FormCountryEdit();
+            fce.CountryData = countryDataBindingSource.Current as CountryData;
+            fce.Show();
+        }
+    }
+}
